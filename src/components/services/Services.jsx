98bc8e1bd4aed14969daebb18aca59cf5489fc0c
Adeweh <dorcasabang@gmail.com>
2023-05-17 15:13:39 +0100

    Service component update

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,101 @@
 import React from 'react'
 import './services.css'
+import {BsFillCloudCheckFill} from 'react-icons/bs'
 
 const Services = () => {
   return (
-   <section id='section'>Services</section>
+   <section id='section'> 
+   <h5>What I Offer</h5>
+   <h2>Services</h2>
+
+   <div className='container services__container'>
+    <article className='service'>
+      <div className='service__head'>
+        <h3 >Data Documentation</h3>
+      </div>
+
+      <ul className='service__list'></ul>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+    </article>
+
+
+    <article className='service'>
+      <div className='service__head'>
+        <h3 >Web Design</h3>
+      </div>
+
+      <ul className='service__list'></ul>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+    </article>
+
+
+    <article className='service'>
+      <div className='service__head'>
+        <h3 >Api Documentation</h3>
+      </div>
+
+      <ul className='service__list'></ul>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <BsFillCloudCheckFill className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+    </article>
+   </div>
+   </section>
   )
 }
 
