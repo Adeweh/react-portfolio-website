d6b950e17bb2d1093d71a75e38099cf51b218229
Adeweh <dorcasabang@gmail.com>
2023-05-18 15:50:11 +0100

    portfolio updates

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assert/IMG1.webp'
+import IMG2 from '../../assert/IMG2.webp'
+import IMG3 from '../../assert/IMG3.webp'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Project</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt=""/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com/' className='btn' target='_blank'>Github</a>
+          <a href='https://dribble.COM/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
+
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt=""/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com/' className='btn' target='_blank'>Github</a>
+          <a href='https://dribble.COM/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
+
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt=""/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com/' className='btn' target='_blank'>Github</a>
+          <a href='https://dribble.COM/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
+
+        </article>
+      </div>
+    </section>
    
   )
 }
