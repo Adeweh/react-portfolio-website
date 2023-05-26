2689969a8a05f09217cab13a307b4a9e3255cff4
Adeweh <dorcasabang@gmail.com>
2023-05-26 18:07:51 +0100

    updates

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './footer.css'
+import {GrInstagram} from 'react-icons/gr'
+import {BsTwitter} from 'react-icons/bs'
+
+
 
 const Footer = () => {
   return (
     <footer>
-      <a href='a' className='footer__logo'>EGATOR</a>
+      <a href='a' className='footer__logo'>SUMMARY</a>
+
       <ul className='permalinks'>
-        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#contacts">Contact</a></li>
+        <li><a href="#contact">Contact</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#service">Service</a></li>
+        <li><a href="#services">Services</a></li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://instagram.com">Instagram</a>
-        <a href="https://twitter.com">Twitter</a>
+        <a href="https://instagram.com" ><GrInstagram/></a>
+        <a href="https://twitter.com"><BsTwitter/></a>
       </div>
     </footer>
   )
